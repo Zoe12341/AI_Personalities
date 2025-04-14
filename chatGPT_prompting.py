44db794402e61f3a6e97ff23d3a818cdc6021b40
Zoe <95237868+Zoe12341@users.noreply.github.com>
2025-04-14 17:53:41 -0400

    adding api prompting ** includes error

--- chatGPT_prompting.py
+++ chatGPT_prompting.py
@@ -1,21 +1,37 @@
 import csv
 import os
+import pandas as pd
 
-#from openai import OpenAI
+#from openai import OpenAI ChatCompletion
+#import openai
 from dotenv import load_dotenv
+from openai import OpenAI
+client = OpenAI()
 
 load_dotenv()
     
 api_key = os.getenv("API_KEY")
 
+df = pd.read_csv('personality_data.csv')
+prompts = df.prompt
 
-"""
-client = OpenAI()
-"""
+df['OpenAI_response'] = None #Add a column with n/a for all rows
+
+""" for prompt in prompts:
+    i = 0
+    completion = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    df.loc[i, 'OpenAI_response'] = completion.choices[0].message.content
+    i +=1 """
 
 
-""" with open('personality_data.csv', 'r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        prompt = "Tell a story in less than 250 words about a person who is ___ in openness, ___ in conscientiousness, ___ in extroversion, low in agreeableness, and ___ in neuroticism. Do not use the words agreeable, neurotic, extroverted, open, or conscientious. Rate whether the main character in this story is high or low on each of the big five personality traits: ___story__"
- """
+
+
+
